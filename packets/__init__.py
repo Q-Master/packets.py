2506dd7903502134975826a7e5429cb850ad635e
Vladimir Berezenko <qmaster2000@gmail.com>
2023-03-27 17:02:37 +0300

    Fixed to newer setup  version

--- packets/__init__.py
+++ packets/__init__.py
@@ -3,3 +3,17 @@ from .packet import *
 from ._packetbase import *
 from .field import *
 from .processors import *
+
+__version__ = '0.6.5'
+
+__title__ = 'packets'
+__description__ = 'Packets system for serialization/deserialization.'
+__url__ = 'https://github.com/Q-Master/packets.py'
+__uri__ = __url__
+__doc__ = f"{__description__} <{__uri__}>"
+
+__author__ = 'Vladimir Berezenko'
+__email__ = 'qmaster2000@gmail.com'
+
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2019-2023 Vladimir Berezenko'
