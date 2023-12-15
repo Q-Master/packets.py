a13c00ad40157f808c2b0952d1f83c9fb9aec7ca
Vladimir Berezenko <qmaster2000@gmail.com>
2023-12-15 18:46:14 +0300

    FIX: fixed bytes type parsing and checking.

--- packets/processors/bytes.py
+++ packets/processors/bytes.py
@@ -17,16 +17,16 @@ class Bytes(FieldProcessor):
         assert isinstance(value, bytes), (value, type(value))
 
     def check_raw(self, value):
-        assert isinstance(value, str), (value, type(value))
+        assert isinstance(value, bytes), (value, type(value))
 
-    def raw_to_py(self, raw_value: str, strict) -> bytes:
+    def raw_to_py(self, raw_value: bytes, strict) -> bytes:
         value = raw_value
         if self._strip is not None:
             value = value.strip(self._strip)
-        return value.encode('utf-8')
+        return value
 
-    def py_to_raw(self, py_value: bytes) -> str:
-        return py_value.decode('utf-8')
+    def py_to_raw(self, py_value: bytes) -> bytes:
+        return py_value
 
     @property
     def my_type(self):
